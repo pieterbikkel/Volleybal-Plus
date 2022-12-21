ebdb0089fc8b7619771464ab28e882a98988c677
pieterbikkel <pieterbikkel@gmail.com>
2022-12-21 19:52:08 +0100

    Initial Commit

---
+++ Volleybal Plus/Volleybal_PlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Volleybal_PlusApp.swift
+//  Volleybal Plus
+//
+//  Created by Pieter Bikkel on 21/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct Volleybal_PlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
